378aac3fa5bc986c66f952c4997ee4488cf34fc7
Bogdan <bogdan198024@yahoo.com>
2020-02-27 09:33:54 +0200

    Update un-dnscrypt-proxy.sh

--- un-dnscrypt-proxy.sh
+++ un-dnscrypt-proxy.sh
@@ -1,18 +1,20 @@
 # usage chmod +x un-dnscrypt-proxy.sh then sudo un-dnscrypt-proxy.sh
 
-echo 'Step 1: Stoping and uninstalling the dnscrypt-proxy service'
+# Step 1: Stoping and uninstalling the dnscrypt-proxy service
 cd ~/dnscrypt-proxy
 ./dnscrypt-proxy -service stop
 ./dnscrypt-proxy -service uninstall
 
-echo 'Step 2: Deleting the dnscrypt-proxy folder'
+# Step 2: Deleting the dnscrypt-proxy folder
 cd
 rm -rf ~/dnscrypt-proxy
 
-echo 'Step 3: Setting DNS for Ethernet to Google DNS'
+# Step 3: Setting DNS for Ethernet and Wi-Fi to Google DNS
 networksetup -setdnsservers Ethernet Empty
+networksetup -setdnsservers Wi-Fi Empty
 dscacheutil -flushcache
 networksetup -setdnsservers Ethernet 8.8.8.8 8.8.4.4
+networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4
 
-echo 'Step 4: Testing the DNS server used'
+# Step 4: Testing the DNS server used
 nslookup cloudflare.com
