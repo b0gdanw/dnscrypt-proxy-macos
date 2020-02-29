1442e0cdf83779ffc8a3cf09c3e548883973a656
Bogdan <bogdan198024@yahoo.com>
2020-02-29 15:40:39 +0200

    Update dnscrypt-proxy.sh

--- dnscrypt-proxy.sh
+++ dnscrypt-proxy.sh
@@ -1,5 +1,4 @@
-# Get the latest release link from https://github.com/DNSCrypt/dnscrypt-proxy/releases/latest
-# usage chmod +x dnscrypt-proxy.sh then sudo dnscrypt-proxy.sh
+# Get the latest dnscrypt-proxy-macos release link from https://github.com/DNSCrypt/dnscrypt-proxy/releases/latest
 
 # Step 1: Creating folders ~/tmp and ~/dnscrypt-proxy
 mkdir ~/tmp
@@ -14,7 +13,7 @@ cp -r ~/tmp/macos/ ~/dnscrypt-proxy
 rm -r ~/tmp
 cd ~/dnscrypt-proxy
 
-# Step 3: Configuring and starting dnscrypt-proxy
+# Step 3: Configuring for Google DoH
 cp example-dnscrypt-proxy.toml dnscrypt-proxy.toml
 sed -i '' "s/# server_names/server_names/" dnscrypt-proxy.toml
 sed -i '' "s/'scaleway-fr', //" dnscrypt-proxy.toml
@@ -23,15 +22,22 @@ sed -i '' "s/, 'yandex'//" dnscrypt-proxy.toml
 # If you want to use Cloudflare too, remove the next line
 sed -i '' "s/, 'cloudflare'//" dnscrypt-proxy.toml
 
+# Disable fallback resolvers (non-https DNS) and netprobe
+sed -i '' "s/fallback_resolvers/# fallback_resolvers/" dnscrypt-proxy.toml
+sed -i '' "s/ignore_system_dns/# ignore_system_dns/" dnscrypt-proxy.toml
+sed -i '' "s/netprobe_timeout/# netprobe_timeout/" dnscrypt-proxy.toml
+sed -i '' "s/netprobe_address/# netprobe_address/" dnscrypt-proxy.toml
+
+# Step 4: Installing and starting the dnscrypt-proxy service
 ./dnscrypt-proxy -service install
 ./dnscrypt-proxy -service start
 
-# Step 4: Setting DNS for Ethernet and Wi-Fi to 127.0.0.1
+# Step 5: Setting DNS for Ethernet and Wi-Fi to 127.0.0.1
 networksetup -setdnsservers Ethernet Empty
 networksetup -setdnsservers Wi-Fi Empty
 dscacheutil -flushcache
 networksetup -setdnsservers Ethernet 127.0.0.1
 networksetup -setdnsservers Wi-Fi 127.0.0.1
 
-# Step 5: Testing the DNS server used
+# Step 6: Testing the DNS server used
 nslookup cloudflare.com
